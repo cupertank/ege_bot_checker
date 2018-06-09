759df0e43ebd6d6e1ae2fddf4c9b2664536b4228
cupertank <34806644+cupertank@users.noreply.github.com>
2018-06-09 12:43:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ while json == None:
       json = get('http://check.ege.edu.ru/api/exam', cookies={'Participant':'634830F905CE5FF5D45969F63A4675D9FA54459F60C7699A1634B5E645CF3FA2743A8E25AE5C0215B082F703835F13A17880CF1AFD638D4DA00EAA11824BAC574A398FAE2C9EE04AA7C1B87BB1BC20C9D5F98251A64526B0B34A85C02A37F64F39A291A5'}).json()
     except:
       print('kek')
-bot.send_message(chat_id=163327661, text='РОБОТАЕТ')
+for mem in members:
+    bot.send_message(chat_id=mem, text='РОБОТАЕТ')
 json = json['Result'].copy()
 while True:
     try:
